ba9d2d70cf45337292a5afb9645c10801d939273
Jérémy THULLIEZ <jeremy.thulliez@zenika.com>
2019-02-07 15:21:14 +0100

    GameDate object

---
+++ Assets/Scripts/Model/GameDate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+/// <summary>
+/// 
+/// This class represent the date in the game.
+/// In the game, time si represented by day, weeks and years. 
+/// 
+/// A week contains 5 days.
+/// A year contains 12 weeks (so ... 5*12 = 60 days)
+/// 
+/// </summary>
+public class GameDate
+{
+    private const int MAX_DAY_IN_WEEK = 5;
+    private const int MAX_WEEK_IN_YEAR = 12;
+
+    private int day;
+    private int week;
+    private int year;
+
+    public GameDate(int _year, int _week, int _day)
+    {
+        if(DateIsInvalid(_year,_week, _day))
+            throw new ArgumentException("Invalid GameDate Values");
+        
+        this.day = _day;
+        this.week = _week;
+        this.year = _year;
+    }
+
+    public void NextDay()
+    {
+        this.day += 1;
+        if(day == MAX_DAY_IN_WEEK)
+        {
+            day = 0;
+            week += 1;
+            if(week == MAX_WEEK_IN_YEAR)
+            {
+                week = 0;
+                year += 1;
+            }
+        }
+    }
+
+    private bool DateIsInvalid(int _year, int _week, int _day)
+    {
+        return _year < 0 ||
+            _week < 0 || _week >= MAX_WEEK_IN_YEAR ||
+            _day < 0 || _day >= MAX_DAY_IN_WEEK;
+    }
+
+    public int Year
+    {
+        get { return year; }
+    }
+
+    public int Week
+    {
+        get { return week; }
+    }
+
+    public int Day
+    {
+        get { return day; }
+    }
+
+    
+}
+
---
+++ Assets/Tests/Model/GameDateTests.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Tests
+{
+    class GameDateTests
+    {
+        [Test]
+        public void Init_ShouldRegister_Values()
+        {
+            GameDate date = new GameDate(1, 2, 3);
+            Assert.AreEqual(1, date.Year);
+            Assert.AreEqual(2, date.Week);
+            Assert.AreEqual(3, date.Day);
+        }
+
+        [Test]
+        public void Init_withNegativeYear_ShouldThrowException()
+        {
+            Assert.Throws<ArgumentException>(delegate { new GameDate(-1, 1, 1); });
+        }
+
+        [Test]
+        public void Init_withNegativeWeek_ShouldThrowException()
+        {
+            Assert.Throws<ArgumentException>(delegate { new GameDate(1, -1, 1); });
+        }
+
+        [Test]
+        public void Init_withTooBigWeek_ShouldThrowException()
+        {
+            Assert.Throws<ArgumentException>(delegate { new GameDate(1, 13, 1); });
+        }
+
+        [Test]
+        public void Init_withTooBigDay_ShouldThrowException()
+        {
+            Assert.Throws<ArgumentException>(delegate { new GameDate(1, 6, -1); });
+        }
+
+        [Test]
+        public void NextDay_ShouldAddADayIfWeekIsNotOver()
+        {
+            GameDate date = new GameDate(0, 0, 0);
+            date.NextDay();
+            Assert.AreEqual(1, date.Day);
+        }
+
+        [Test]
+        public void NextDay_ShouldResetDayAndUpdateWeekIfWeekIsOver()
+        {
+            GameDate date = new GameDate(0, 0, 4);
+            date.NextDay();
+            Assert.AreEqual(0, date.Day);
+            Assert.AreEqual(1, date.Week);
+        }
+
+        [Test]
+        public void NextDay_Should_ResetDay_AndResetWeek_AndUpdateYear_IfWeekIsOverAndYearIsOver()
+        {
+            GameDate date = new GameDate(0, 11, 4);
+            date.NextDay();
+            Assert.AreEqual(0, date.Day);
+            Assert.AreEqual(0, date.Week);
+            Assert.AreEqual(1, date.Year);
+        }
+    }
+}
