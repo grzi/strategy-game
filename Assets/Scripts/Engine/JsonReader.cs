affdde3c0ddc486d10a0842c7b0ff4b20eb7aab3
Jérémy Thulliez <jeremy.thulliez@zenika.com>
2019-02-07 22:24:59 +0100

    Added json reader

---
+++ Assets/Scripts/Engine/JsonReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+/// <summary>
+/// Json parser so as to read and write date from / to json
+/// </summary>
+/// <typeparam name="T">Json read will be parsed to type T</typeparam>
+public class JsonReader<T> {
+    public static T read(String path) {
+        JObject tmp = null;
+        
+        using (StreamReader file = File.OpenText(@path))
+        using (JsonTextReader reader = new JsonTextReader(file)) {
+            tmp = (JObject)JToken.ReadFrom(reader);
+        }
+
+        if(tmp != null) {
+            return (T)tmp.ToObject(typeof(T));
+        }
+
+        throw new JsonReaderException("Impossible to read the json at : " + @path);
+    }
+}
+
---
+++ Assets/Tests/Engine/JsonReaderTests.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Tests {
+    class JsonReaderTests {
+
+        class Test {
+            public int Property1 { get; set; }
+            public string Property2 { get; set; }
+            public bool Property3 { get; set; }
+        }
+
+        [Test]
+        public  void read_valid_json_should_return_object() {
+            Test test = JsonReader<Test>.read("Assets/Tests/Engine/test_ok.json");
+            Assert.AreEqual(5, test.Property1);
+        }
+
+        [Test]
+        public void read_invalid_json_should_throw_exception() {
+            Assert.Throws<JsonReaderException>(
+                delegate {
+                    JsonReader<Test>.read("Assets/Tests/Engine/test_ko.json");
+                }
+            );
+        }
+
+    }
+}
